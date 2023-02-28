c6cc078ee48b0184610c29d69b29c0bb70c25a0c
Joaquín Frías <119880493+Joaquin1245@users.noreply.github.com>
2023-02-28 15:39:57 -0300

    Se agregan animación a las barras de habilidades

--- app.js
+++ app.js
@@ -19,3 +19,79 @@ for (let i = 0; i < links.length; i++) {
         menu_visible = false;
     }
 }
+
+// Creo las barritas de una barra particular identificada por su id //
+function crearBarra(id_barra){
+    for (i=0;i<=16;i++){
+        let div = document.createElement("div");
+        div.className = "e"
+        id_barra.appendChild(div);
+    }
+}
+
+// selecciono todas las barras generales para luego manipularlas //
+let html = document.getElementById("html");
+crearBarra(html);
+let javascript = document.getElementById("javascript");
+crearBarra(javascript);
+let wordpress = document.getElementById("wordpress");
+crearBarra(wordpress);
+let photoshop = document.getElementById("photoshop");
+crearBarra(photoshop);
+let php = document.getElementById("php");
+crearBarra(php);
+let illustrator = document.getElementById("illustrator");
+crearBarra(illustrator);
+
+// Ahora voy a guardar la cantidad de barritas que se van a ir pintando por cada barra
+// para eso utilizo un arreglo, cada posición pertenece a un elemento
+// comienzan en -1 porque no tiene ninguna pintada al iniciarse
+
+let contadores = [-1,-1,-1,-1,-1,-1];
+// esta variable la voy a utilizar de bandera para saber si ya ejecuto la animación
+let entro = false;
+
+//Función que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var habilidades = document.getElementById("habilidades");
+    var distancia_skills = window.innerHeight - habilidades.getBoundingClientRect().top;
+    if(distancia_skills>=300 && entro==false){
+        entro = true;
+        const intervalHtml = setInterval(function(){
+            pintarBarra(html, 16, 0, intervalHtml);
+        },100);
+        const intervalJavascript = setInterval(function(){
+            pintarBarra(javascript, 11, 1, intervalJavascript);
+        },100);
+        const intervalWordpress = setInterval(function(){
+            pintarBarra(wordpress, 15, 2, intervalWordpress);
+        },100);
+        const intervalPhotoshop = setInterval(function(){
+            pintarBarra(photoshop, 14, 3, intervalPhotoshop);
+        },100);
+        const intervalPhp = setInterval(function(){
+            pintarBarra(php, 16, 4, intervalPhp);
+        },100);
+        const intervalIllustrator = setInterval(function(){
+            pintarBarra(illustrator, 12, 5, intervalIllustrator);
+        },100);
+    }
+}
+
+// lleno una barra particular con la cantidad indicada
+function pintarBarra(id_barra, cantidad, indice, interval){
+    contadores[indice]++;
+    x = contadores[indice];
+    if(x < cantidad){
+        let elementos = id_barra.getElementsByClassName("e");
+        elementos[x].style.backgroundColor = "#940253"
+    } else{
+        clearInterval(interval);
+    }
+}
+
+// detecto el scrolling del mouse para aplicar la animación de la barra
+
+window.onscroll = function(){
+    efectoHabilidades();
+}
